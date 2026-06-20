6f76ce92c1d2f6763958490a1b651a400deddaf8
Jaime Criales <jaimecriales8@icloud.com>
2026-06-20 15:22:38 -0500

    Remove page title from browser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Verificación SDS 2026",
+  title: " ",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
