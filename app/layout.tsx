94f0459dacbb270eac0dc7b819d44b7689a818b4
Jaime Criales <jaimecriales8@icloud.com>
2026-06-20 15:24:39 -0500

    Tab title: Actualización

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: " ",
+  title: "Actualización",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
